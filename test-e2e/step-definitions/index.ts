09e5a878d1486b95e77a32a91915bafc44c30c8c
Alexandr_Legchilov <alexandr_legchilov@epam.com>
2024-10-24 17:30:52 +0300

    core v2 migration

--- test-e2e/step-definitions/index.ts
+++ test-e2e/step-definitions/index.ts
@@ -2,10 +2,11 @@ import {Then, When, After, Before} from '@cucumber/cucumber';
 import memory from '@qavajs/memory';
 import * as path from 'path';
 import {expect} from 'chai';
-import {writeFileSync, readdirSync, unlinkSync} from "node:fs";
+import {writeFileSync, existsSync, readdirSync, unlinkSync, mkdirSync} from "node:fs";
 
 When('I drop file {string} to {string} after {int} ms', function (file, dir, delay) {
     setTimeout(() => {
+        existsSync(dir) || mkdirSync(dir);
         writeFileSync(path.join(dir, file), 'content', 'utf-8')
     }, delay);
 });

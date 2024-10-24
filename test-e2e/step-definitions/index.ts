85e985dacbff4fee6fc6bfe32df679199a266d3b
Alexandr_Legchilov <alexandr_legchilov@epam.com>
2024-10-24 16:25:27 +0300

    core v2 migration

--- test-e2e/step-definitions/index.ts
+++ test-e2e/step-definitions/index.ts
@@ -1,7 +1,7 @@
-import { Then, When, After } from '@cucumber/cucumber';
+import {Then, When, After} from '@cucumber/cucumber';
 import memory from '@qavajs/memory';
 import * as path from 'path';
-import { expect } from 'chai';
+import {expect} from 'chai';
 import {writeFileSync, readdirSync} from "node:fs";
 import {unlinkSync} from "fs-extra";
 
@@ -11,22 +11,19 @@ When('I drop file {string} to {string} after {int} ms', async function (file, di
     }, delay);
 });
 
-Then('I expect {string} memory value to be equal {string}', async function(actual, expected) {
+Then('I expect {string} memory value to be equal {string}', async function (actual, expected) {
     const actualValue = memory.getValue(actual);
     const expectedValue = memory.getValue(expected);
     expect(actualValue).to.eql(expectedValue);
 });
 
-Then('I expect {string} memory value to contain {string}', async function(actual, expected) {
+Then('I expect {string} memory value to contain {string}', async function (actual, expected) {
     const actualValue = memory.getValue(actual);
     const expectedValue = memory.getValue(expected);
     expect(actualValue).to.contain(expectedValue);
 });
 
 After(async function () {
-    const dir ='./test-e2e/folder';
+    const dir = './test-e2e/folder';
     readdirSync(dir).forEach(f => unlinkSync(`${dir}/${f}`));
 });
-
-
-
